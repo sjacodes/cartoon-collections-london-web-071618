bda7a2c976cba4a5213f0c03c764dba4700f9216
Sarah Jacob <smjacob91@gmail.com>
2018-07-05 20:56:39 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,12 @@ def long_planeteer_calls(calls)
 end
 
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+
+
+
+
+
+
+
+
+
