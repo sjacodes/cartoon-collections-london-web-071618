28d76aea3bb941a94c807a0b8df6c6561001d5a7
Sarah Jacob <smjacob91@gmail.com>
2018-07-05 20:13:01 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf_names)
+  list_of_dwarf_names = []
+  dwarf_names.each_with_index do |names, index|
+   list_of_dwarf_names.push(#{names)
 end
 
 def summon_captain_planet# code an argument here
